bb2eb952bdc4f29dc1af00f2ef570cf8fa87e819
Ramsha <fnuramsha@gmail.com>
2025-04-28 10:51:21 +0200

    fixed issues related to breadcrumb

--- fetch-board/src/Components/breadcrumb.jsx
+++ fetch-board/src/Components/breadcrumb.jsx
@@ -5,15 +5,17 @@ const Breadcrumb = () => {
   const location = useLocation(); // returns the current location
 
   let breadCrumb = null;
+
   if (location?.pathname?.includes("/todo/")) {
+    console.log("I am on Todo's", location.pathname);
     breadCrumb = "ToDos/ToDoSInformation";
-  } else if (location?.pathname?.includes("/post")) {
+  } else if (location?.pathname?.includes("/post/")) {
     breadCrumb = "Posts/PostsInformation";
   }
   if (breadCrumb != null) {
     return (
-      <nav aria-label="breadcrumb" className="px-2">
-        <ol className="breadcrumb mr-80">
+      <nav aria-label="breadcrumb justify-content-center" className="px-2">
+        <ol className="breadcrumb mr-80 align-middle">
           <li className="breadcrumb-item active" aria-current="page">
             <Link to="/">Home</Link>/{breadCrumb}
           </li>
